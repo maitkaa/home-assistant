3e175f2f8aedeb58d43d97b49885d76efee0e9ad
mait.kaasik <mait.kaasik@helmes.com>
2024-03-26 20:02:11 +0200

    fix dark mode

--- app/root.tsx
+++ app/root.tsx
@@ -52,7 +52,6 @@ export function App() {
         <Outlet/>
         <ScrollRestoration/>
         <Scripts/>
-        <LiveReload/>
         </body>
         </html>
     )
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,28 +10,20 @@ export const meta: MetaFunction = () => {
 import {
   Activity,
   ArrowUpRight,
-  CircleUser,
   CreditCard,
   DollarSign,
   Menu,
   Package2,
-  Search,
   Users,
 } from "lucide-react"
 import {Link} from '@remix-run/react';
 import {Sheet, SheetContent, SheetTrigger} from '../@/components/ui/sheet';
 import {Button} from '../@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent, DropdownMenuItem,
-  DropdownMenuLabel, DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from '../@/components/ui/dropdown-menu';
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from '../@/components/ui/card';
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from '../@/components/ui/table';
 import {Badge} from '../@/components/ui/badge';
 import {Avatar, AvatarFallback, AvatarImage} from '../@/components/ui/avatar';
-import {Input} from '../@/components/ui/input';
+import {ModeToggle} from '../components/mode-toggle';
 
 export default function Dashboard() {
   return (
@@ -39,41 +31,10 @@ export default function Dashboard() {
         <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
           <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
             <Link
-                to="#"
-                className="flex items-center gap-2 text-lg font-semibold md:text-base"
-            >
-              <Package2 className="h-6 w-6" />
-              <span className="sr-only">Acme Inc</span>
-            </Link>
-            <Link
-                to="#"
+                to="/"
                 className="text-foreground transition-colors hover:text-foreground"
             >
               Dashboard
-            </Link>
-            <Link
-                to="#"
-                className="text-muted-foreground transition-colors hover:text-foreground"
-            >
-              Orders
-            </Link>
-            <Link
-                to="#"
-                className="text-muted-foreground transition-colors hover:text-foreground"
-            >
-              Products
-            </Link>
-            <Link
-                to="#"
-                className="text-muted-foreground transition-colors hover:text-foreground"
-            >
-              Customers
-            </Link>
-            <Link
-                to="#"
-                className="text-muted-foreground transition-colors hover:text-foreground"
-            >
-              Analytics
             </Link>
           </nav>
           <Sheet>
@@ -127,32 +88,9 @@ export default function Dashboard() {
             </SheetContent>
           </Sheet>
           <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-            <form className="ml-auto flex-1 sm:flex-initial">
-              <div className="relative">
-                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                <Input
-                    type="search"
-                    placeholder="Search products..."
-                    className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-                />
-              </div>
-            </form>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="secondary" size="icon" className="rounded-full">
-                  <CircleUser className="h-5 w-5" />
-                  <span className="sr-only">Toggle user menu</span>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Settings</DropdownMenuItem>
-                <DropdownMenuItem>Support</DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Logout</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <div className="ml-auto flex-1 sm:flex-initial">
+              <ModeToggle />
+            </div>
           </div>
         </header>
         <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
